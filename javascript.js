6f38b53aafcf9f14794e3ad7af0314d65892eb71
edent <github.com@shkspr.mobi>
2017-10-26 20:30:09 +0100

    Added Evernote

--- javascript.js
+++ javascript.js
@@ -8,7 +8,7 @@ var items = [
   'pinterest', 'snapchat', 'telegram', 'vk', 'wordpress', 'meetup',
   'line', 'lastpass', 'windows', 'digidentity', 'ubuntu', 'bitbucket',
   'apple', 'npm', 'docker', 'symantec', 'yubico', 'keybase',
-  'ebay'
+  'ebay', 'evernote'
 ];
 
 var container = document.querySelector('.icons');
