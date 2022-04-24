3c027afd73d1c2d83d5fb99324967f1bf0fdbaa2
Ethan Chapman <Eiim@users.noreply.github.com>
2022-04-24 15:55:51 -0400

    Test readme size

--- test/test.js
+++ test/test.js
@@ -3,15 +3,21 @@ const fs = require("fs")
 const fetch = (...args) => import('node-fetch').then(({default: fetch}) => fetch(...args));
 
 const svgDir = __dirname + "/../images/svg/"
+const readme = __dirname + "/../README.md"
 
+const readmeLines = fs.readFileSync(readme).toString().split("\n")
 const files = fs.readdirSync(svgDir)
+
+const readmeRegex = new RegExp("<br>(\\d{1,4}) Bytes<\/td>")
 
 files.forEach((filename, i) => {
     if (! filename.endsWith(".svg")) {return}
+	if (i > 3) {return}
     const filepath = svgDir + filename
     describe(filename, async () => {
         it("should exists", () => {
-             expect(fs.existsSync(filepath)).to.be.true
+            console.log("  ", i + 1, "/", files.length)
+            expect(fs.existsSync(filepath)).to.be.true
         })
 
         it("should be under 1KB", () => {
@@ -24,11 +30,22 @@ files.forEach((filename, i) => {
                     body: fs.readFileSync(filepath, "utf8").replace(/aria-label="[^"]+"/g, ""),
                     headers: {"Content-Type": "application/xml"}
                 })
-                .then(res => (console.log(i + 1, "/", files.length), res))
                 .then(res => res.text())
                 .then(res => {
                     expect(res).to.be.empty
                 })
         })
+        
+        it("should match readme size", () => {
+            for(line of readmeLines) {
+                if(line.includes("/svg/"+filename)) {
+					console.log(line)
+                    const size = parseInt(line.match(readmeRegex)[1])
+                    expect(fs.statSync(filepath).size).to.equal(size)
+                    break
+                }
+            }
+        })
     })
+
 })
