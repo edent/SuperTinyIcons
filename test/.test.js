7d4632929baad5d03e471470c8bfb482107185f0
kuubeu <kuubeu@gmail.com>
2023-09-14 18:07:08 +0200

    Node.js →  Bun, replaced fetching w3c.org with a validator run locally (vnu)

---
+++ test/.test.js
@@ -0,0 +1,83 @@
+import { describe, expect, test } from 'bun:test'
+import { readdirSync, statSync } from 'fs'
+import { execFileSync } from 'child_process'
+import vnu from 'vnu-jar'
+
+const svgDir = 'images/svg/'
+
+
+printBold('\nGetting file sizes from README')
+
+const readmeLines = (await Bun.file('README.md').text()).split('\n')
+const readmeRegex = /<td>.*\/svg\/(.*\.svg).*<br>(\d+) bytes/
+const fileSizeMap = new Map()
+
+for (const line of readmeLines) {
+  const match = readmeRegex.exec(line)
+  if (match) {
+    const filename = match[1]
+    const size = parseInt(match[2])
+    fileSizeMap.set(filename, size)
+  }
+}
+
+
+printBold('\nValidating SVGs with the W3C validator (vnu)')
+
+const validationErrorsMap = new Map()
+
+const args = [
+  '-jar', vnu,
+  '--skip-non-svg',
+  '--filterpattern', '.*aria-label.*',
+  svgDir
+]
+
+try {
+  execFileSync('java', args)
+} catch (error) {
+  const errors = error.message.split('\n').filter(line => line)
+  for (const error of errors) {
+    const match = /svg\/((.*\.svg).*)/.exec(error)
+    if (match) validationErrorsMap.set(match[2], match[1])
+  }
+}
+
+
+printBold('\nRunning tests')
+
+const files = readdirSync(svgDir)
+
+files.forEach(filename => {
+  if (!filename.endsWith('.svg')) return
+
+  const filesize = statSync(svgDir + filename).size
+  const readmesize = fileSizeMap.get(filename)
+  fileSizeMap.delete(filename)
+
+  describe(filename, () => {
+    test('should be under 1KB', () => {
+      expect(filesize).toBeLessThan(1024)
+    })
+
+    test('should be included in readme', () => {
+      expect(readmesize).toBeNumber()
+    })
+
+    test('should match readme size', () => {
+      expect(filesize).toBe(readmesize)
+    })
+
+    test('should be validated by the w3c validator', () => {
+      expect(validationErrorsMap.get(filename)).toBeFalsy()
+    })
+  })
+})
+
+test('all files in readme should exist', () => {
+  expect(fileSizeMap).toBeEmpty()
+})
+
+function printBold(text) {
+  console.log('\x1b[1m%s\x1b[0m', text)
+}
