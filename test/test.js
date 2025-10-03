907b380bc1260aaf812ccc05f0432b20ac657431
Terence Eden <edent@users.noreply.github.com>
2025-10-03 21:07:24 +0100

    Fix failing tests

--- test/test.js
+++ test/test.js
@@ -2,7 +2,7 @@ const expect = require("chai").expect
 const fs = require("fs")
 const fetch = (...args) => import('node-fetch').then(({default: fetch}) => fetch(...args));
 
-const svgDir = __dirname + "/../images/svg/"
+const svgDir = __dirname + "/../"
 const readme = __dirname + "/../README.md"
 
 const readmeLines = fs.readFileSync(readme).toString().split("\n")
@@ -13,25 +13,26 @@ console.log('Changed files:', changedFiles);
 
 const changedFilesArray = changedFiles ? changedFiles.split(/\s+/) : [];
 console.log('Changed files as array:', changedFilesArray);
-/*
-const files= changedFiles ? changedFiles.filter(file.endswith('.svg')).map(file => file.split('/').pop()) : fs.readdirSync(svgDir);
 
 const readmeRegex = new RegExp("<br>(\\d{1,4}) Bytes<\/td>")
 
 const delay = ms => new Promise(res => setTimeout(res, ms));
 
-files.forEach((filename, i) => {
+changedFilesArray.forEach((filename, i) => {
+    console.log(`Filename is: ${filename}`);
+    
     if (! filename.endsWith(".svg")) {return}
+    
     const filepath = svgDir + filename
+    console.log(`Filepath is: ${filepath}`);
+
     if(!fs.existsSync(filepath)) {
         console.log(`Skipping deleted file: ${filename}`);
         return;
     }
+    console.log('SVG Contents: ', fs.readFileSync(filepath, "utf8"));
+
     describe(filename, async () => {
-        it("should exists", () => {
-            console.log("  ", i + 1, "/", files.length)
-            expect(fs.existsSync(filepath)).to.be.true
-        })
 
         it("should be under 1KB", () => {
             expect(fs.statSync(filepath).size).to.be.lessThan(1024)
@@ -59,11 +60,5 @@ files.forEach((filename, i) => {
                 }
             }
         })
-
-        
     })
-
 })
-
-
-*/
